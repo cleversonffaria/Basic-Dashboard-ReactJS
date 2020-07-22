cd2f557ba238737ae4e49e658574e5a35a453a59
cleversonffaria <cleversonffaria@gmail.com>
2020-07-22 14:59:18 -0300

    Iniciando Projeto - Redux configurado

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function Dashboard() {
+  return <div>Dashboard</div>;
+}
+
+export default Dashboard;
---
+++ src/pages/Login/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import { useSelector, useDispatch } from "react-redux";
+
+// import { Container } from './styles';
+
+// import { Creators as AuthActions } from "../../store/ducks/auth";
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <button onClick={() => {}}>Entrar</button>
+    </div>
+  );
+}
+
+export default Login;
